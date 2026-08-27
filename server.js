bf4847728a03067104e04c4571be36aad02b7dfa
Kovaliosky <abadioguilherme76@gmail.com>
2026-08-27 20:01:17 -0300

    Update Google OAuth2 client ID in server.js

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const sqlite3 = require('sqlite3').verbose();
 const { OAuth2Client } = require('google-auth-library');
 
 const app = express();
-const client = new OAuth2Client('SEU_CLIENT_ID_DO_GOOGLE.apps.googleusercontent.com');
+const client = new OAuth2Client('1056756025173-svte8g5subniiocusau7umuhng53k5od.apps.googleusercontent.com');
 
 app.use(cors());
 app.use(express.json());
